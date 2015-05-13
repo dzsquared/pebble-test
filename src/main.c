08ea266a236b25626923363f41c1f083cd0871cd
DrewSK <dzsquared@users.noreply.github.com>
2015-05-13 13:03:24 -0500

    quadfont and background
    working with custom font and background

--- src/main.c
+++ src/main.c
@@ -2,6 +2,9 @@
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void update_time() {
   // Get a tm structure
@@ -25,10 +28,21 @@ static void update_time() {
 }
 
 static void main_window_load(Window *window) {
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
+
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_QUAD_42));
+
+  // Apply to TextLayer
+  text_layer_set_font(s_time_layer, s_time_font);
   // Create time TextLayer
   s_time_layer = text_layer_create(GRect(0, 55, 144, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
   //text_layer_set_text(s_time_layer, "00:00");
 
   // Improve the layout to be more like a watchface
@@ -39,8 +53,17 @@ static void main_window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 static void main_window_unload(Window *window) {
+  // Destroy GBitmap
+  gbitmap_destroy(s_background_bitmap);
+  
+  // Destroy BitmapLayer
+  bitmap_layer_destroy(s_background_layer);
+  
+    // Unload GFont
+    fonts_unload_custom_font(s_time_font);
     // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+  
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
